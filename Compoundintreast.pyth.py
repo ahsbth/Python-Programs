f15344ce3657a0d44d5d20671547834d8f20cd25
ahsbth <asraj48342@gmail.com>
2023-04-20 16:32:27 +0530

    Evenodd

--- Compoundintreast.pyth.py
+++
@@ -1,8 +0,0 @@
-import math
-print('Enter principle,rate and time=')
-p=float(input())
-r=float(input())
-t=float(input())
-A=p*(math.pow((1+r/100),t))
-ci=A-p
-print('Compund intreast=',ci)
---
+++ condition.py
@@ -0,0 +1,6 @@
+print("Enter a no=");
+a=int(input())
+if(a%2==0):
+    print("Even")
+else:
+    print("Odd")
